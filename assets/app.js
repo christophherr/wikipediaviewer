01c32a4b5c727b1b6b13b0f3021ae5ba743fb1f5
Christoph Herr <info@christophherr.com>
2017-12-08 20:37:48 -0500

    Updated code style

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,26 @@
-$(document).ready(function() {
-
-  $("#clearButton").click( function() {
-    $("#searchTerm").val('');
-    $(".articles-list").html("");
+// @ts-check @ignore $
+$(document).ready(() => {
+  $('#clearButton').click(() => {
+    $('#searchTerm').val('');
+    $('.articles-list').html('');
   });
 
-  $("#search").submit( function(event) {
+  $('#search').submit((event) => {
     event.preventDefault();
-    var searchTerm = $("#searchTerm").val();
-    var apiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + searchTerm + "&callback=?";
+    const searchTerm = $('#searchTerm').val();
+    const apiUrl = `https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=${
+      searchTerm
+    }&callback=?`;
     console.log(apiUrl);
 
-    $.getJSON(apiUrl, function(data) {
-      $(".articles-list").html("");
-      for (var i = 0; i < data[1].length; i++) {
-        var newItem = $("<a href=" + data[3][i] + " target='_blank'><li><b>" + data[1][i] + "</b> - " + data[2][i] + "</li></a>").hide();
+    $.getJSON(apiUrl, (data) => {
+      $('.articles-list').html('');
+      for (let i = 0; i < data[1].length; i++) {
+        const newItem = $(`<a href='${data[3][i]}'
+        target='_blank'><li><b>${data[1][i]}</b> -
+        ${data[2][i]}</li></a>`).hide();
 
-        $(".articles-list").append(newItem);
+        $('.articles-list').append(newItem);
         newItem.slideDown('slow');
       }
     });
